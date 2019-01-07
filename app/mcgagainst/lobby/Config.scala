736a7715fe2ffc9edcb685fe33322a6703502d2d
Michael J Peppers <mcgdevfunk824@protonmail.com>
2019-01-07 12:53:30 -0800

    Create Config.scala

---
+++ app/mcgagainst/lobby/Config.scala
@@ -0,0 +1,35 @@
+package mcgagainst.lobby
+
+import mcgagainst.models.Game.{Config => ConfigModel}
+import mcgagainst.models.cardcast.CardcastDeck
+import mcgagainst.notifications.Notifiers
+
+class Config(notifiers: Notifiers) {
+
+  var decks: List[CardcastDeck] = List()
+  var houseRules: Set[String] = Set()
+  var password: Option[String] = None
+
+  def config = ConfigModel(decks.map(deck => deck.info), houseRules, password)
+
+  def addDeck(deck: CardcastDeck): Unit = {
+    decks = decks :+ deck
+    notifiers.configChange(config)
+  }
+
+  def addHouseRule(rule: String): Unit = {
+    houseRules = houseRules + rule
+    notifiers.configChange(config)
+  }
+
+  def removeHouseRule(rule: String): Unit = {
+    houseRules = houseRules - rule
+    notifiers.configChange(config)
+  }
+
+  def setPassword(newPass: Option[String]) = {
+    password = newPass.flatMap(pass => if (pass.isEmpty) None else newPass)
+    notifiers.configChange(config)
+  }
+
+}
